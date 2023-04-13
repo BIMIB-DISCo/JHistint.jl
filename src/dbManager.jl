82e5ca0adf1897b97bcee0d0645bdfecf9923134
Niccolò Mandelli <91435823+niccolo99mandelli@users.noreply.github.com>
2023-04-13 15:44:51 +0200

    Add files via upload

--- src/dbManager.jl
+++ src/dbManager.jl
@@ -64,7 +64,7 @@ function insert_record_DB(col_name::AbstractString,
     # Extract data from image slide
     # filepath_svs = "C:/Users/nicom/Desktop/segmentation/TCGA-OR-A5J1-01A-01-TS1.CFE08710-54B8-45B0-86AE-500D6E36D8A5_001.tif"
     svs_image = load(filepath_svs)
-    println("test")
+    imshow(svs_image)
     # svs_image = ImageMagick.load_(svs_image_byte)
 
     # Connect to DB
