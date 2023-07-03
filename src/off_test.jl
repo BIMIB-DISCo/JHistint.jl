14690a9678bbfef03b00350a05f0a429d7a662e1
niccolo99mandelli <n.mandelli2@campus.unimib.it>
2023-07-04 01:34:00 +0200

    update test

--- src/off_test.jl
+++ src/off_test.jl
@@ -27,17 +27,24 @@ using NetworkLayout
 using CairoMakie
 using GraphMakie
 
+# plotting
+using GraphPlot
+using Karnak
+using Colors
+using Luxor
+using Compose
+using LinearAlgebra
 
 """
-    c = segment_pixel_count(seg, l)
-
-Returns the count of pixels that are assigned label `l`. If no label is
-supplied, it returns a Dict(label=>pixel_count) of all the labels.
+`SegmentedImage` type contains the index-label mapping, assigned labels,
+segment mean intensity and pixel count of each segment.
+struct SegmentedImage{T<:AbstractArray,U<:Union{Colorant,Real}}
+    image_indexmap::T
+    segment_labels::Vector{Int}
+    segment_means::Dict{Int,U}
+    segment_pixel_count::Dict{Int,Int}
+end
 """
-segment_pixel_count(seg::SegmentedImage, l::Int16) = seg.segment_pixel_count[l]
-segment_pixel_count(seg::SegmentedImage) = seg.segment_pixel_count
-
-
 @static if Base.VERSION <= v"1.0.5"
     # https://github.com/JuliaLang/julia/pull/29442
     _oneunit(::CartesianIndex{N}) where {N} = _oneunit(CartesianIndex{N})
@@ -50,6 +57,12 @@ end
 _colon(I::CartesianIndex{N}, J::CartesianIndex{N}) where N =
     CartesianIndices(map((i,j) -> i:j, Tuple(I), Tuple(J)))
 
+struct Point{T}
+    x::T
+    y::T
+    z::T
+end
+
 """
     G, vert_map = region_adjacency_graph(seg, weight_fn)
 
@@ -61,7 +74,6 @@ along with a Dict(label=>vertex) map. `weight_fn` is used to assign weights to t
 Returns a real number corresponding to the weight of the edge between label1 and label2.
 
 """
-
 function region_adjacency_graph(s::SegmentedImage, weight_fn::Function)
 
     function neighbor_regions!(df_cartesian_indices::AbstractArray, G::SimpleWeightedGraph, visited::AbstractArray, s::SegmentedImage, I::CartesianIndex)
@@ -108,8 +120,8 @@ function region_adjacency_graph(s::SegmentedImage, weight_fn::Function)
     visited  = fill(false, axes(s.image_indexmap))                           # Array to mark the pixels that are already visited
     G        = SimpleWeightedGraph()                                         # The region_adjacency_graph
     vert_map = Dict{Int,Int}()                                               # Map that stores (label, vertex) pairs
-    # Build object for dataframe
-    df = DataFrame()
+    # Build object for label (vertex) dataframe
+    df_label = DataFrame()
     df_cartesian_indices = []
     df_color_indices = []
     # add vertices to graph
@@ -148,46 +160,28 @@ function region_adjacency_graph(s::SegmentedImage, weight_fn::Function)
     for i in s.segment_labels
         push!(df_color_indices, s.segment_means[i])
     end
-    df.label = s.segment_labels
-    df.position_label = df_cartesian_indices
-    df.color_label = df_color_indices
-    G, vert_map, df
+    df_label.label = s.segment_labels
+    df_label.position_label = df_cartesian_indices
+    df_label.color_label = df_color_indices
+    G, vert_map, df_label
 end
 
-function save_adjacency_matrix(matrix::Matrix{Int64}, filepath_matrix::AbstractString)
-    # Open txt file
-    f = open(filepath_matrix, "w")
-    # Write matrix dimensions
-    n_rows, n_cols = size(matrix)
-    matrix_string = ""
-    for i in 1:n_rows
-        write(f, " ")
-        for j in 1:n_cols
-            matrix_string = string("", matrix[i, j], "  ")
-            write(f, matrix_string)
-        end
-        write(f, "\n")
-    end
-    close(f)
-end
+"""
+    weighted_graph_to_adjacency_matrix(G::SimpleWeightedGraph{Int64, Float64}, n::Int64)
 
-function save_adjacency_matrix(matrix::Matrix{Float32}, filepath_matrix::AbstractString)
-    # Open txt file
-    f = open(filepath_matrix, "w")
-    # Write matrix dimensions
-    n_rows, n_cols = size(matrix)
-    matrix_string = ""
-    for i in 1:n_rows
-        write(f, " ")
-        for j in 1:n_cols
-            matrix_string = string("", matrix[i, j], "  ")
-            write(f, matrix_string)
-        end
-        write(f, "\n")
-    end
-    close(f)
-end
+Converts a weighted graph represented as a `SimpleWeightedGraph` into an unweighted boolean adjacency matrix.
 
+# Arguments:
+- `G::SimpleWeightedGraph{Int64, Float64}`: Weighted graph represented as a `SimpleWeightedGraph` with integer vertex labels and floating-point edge weights.
+- `n::Int64`: Number of nodes in the adjacency matrix.
+
+# Return value:
+- `adjacency_matrix`: `Matrix{Int64}` boolean adjacency matrix.
+
+# Notes:
+The function returns an `n` x `n` adjacency matrix representing the unweighted graph. If nodes `i` and `j` are adjacent,
+the adjacency matrix will contain a value of `1` at position `(i,j)` and `(j,i)`. Otherwise, the adjacency matrix will contain a value of `0`.
+"""
 function weighted_graph_to_adjacency_matrix(G::SimpleWeightedGraph{Int64, Float64}, n::Int64)
     adjacency_matrix = zeros(Int, n, n)
     for i in 1:n
@@ -201,6 +195,15 @@ function weighted_graph_to_adjacency_matrix(G::SimpleWeightedGraph{Int64, Float6
     return adjacency_matrix
 end
 
+"""
+    weighted_graph_to_adjacency_matrix_weight(G::SimpleWeightedGraph{Int64, Float64}, n::Int64)
+
+# Arguments:
+
+# Return value:
+
+# Notes:
+"""
 function weighted_graph_to_adjacency_matrix_weight(G::SimpleWeightedGraph{Int64, Float64}, n::Int64)
     adjacency_matrix = zeros(Float32, n, n)
     for i in 1:n
@@ -221,6 +224,15 @@ function weighted_graph_to_adjacency_matrix_weight(G::SimpleWeightedGraph{Int64,
     return adjacency_matrix
 end
 
+"""
+    build_dataframe_as_edgelist(mat::Matrix{Int64})
+
+# Arguments:
+
+# Return value:
+
+# Notes:
+"""
 function build_dataframe_as_edgelist(mat::Matrix{Int64})
     df = DataFrame()
     df_origin_column = []
@@ -229,7 +241,7 @@ function build_dataframe_as_edgelist(mat::Matrix{Int64})
     n = size(mat, 1)
     for i in 1:n
         for j in 1:i
-            if mat[i,j] != -1
+            if mat[i,j] != 0
                 push!(df_origin_column, i)
                 push!(df_destination_column, j)
                 push!(df_weight_column, mat[i,j])
@@ -242,6 +254,15 @@ function build_dataframe_as_edgelist(mat::Matrix{Int64})
     return df
 end
 
+"""
+    build_dataframe_as_edgelist(mat::Matrix{Float32})
+
+# Arguments:
+
+# Return value:
+
+# Notes:
+"""
 function build_dataframe_as_edgelist(mat::Matrix{Float32})
     df = DataFrame()
     df_origin_column = []
@@ -263,8 +284,62 @@ function build_dataframe_as_edgelist(mat::Matrix{Float32})
     return df
 end
 
-# J-SPACE FUNCTIONS
-# in DataFrameGraphBridge.jl
+"""
+    save_adjacency_matrix(matrix::Matrix{Int64}, filepath_matrix::AbstractString)
+
+Function to save an adjacency matrix represented as an integer matrix to a text file.
+
+# Arguments:
+- `matrix::Matrix{Int64}`: The integer matrix representing the adjacency matrix.
+- `filepath_matrix::AbstractString`: The file path represented as a string indicating where to save the matrix.
+
+# Notes
+The function opens the file specified by the `filepath_matrix` path in write mode and writes the matrix in adjacency matrix format,
+where each row represents the adjacent nodes of a node. The numbers in the matrix are separated by spaces.
+"""
+function save_adjacency_matrix(matrix::Matrix{Int64}, filepath_matrix::AbstractString)
+    # Open txt file
+    f = open(filepath_matrix, "w")
+    # Write matrix dimensions
+    n_rows, n_cols = size(matrix)
+    matrix_string = ""
+    for i in 1:n_rows
+        write(f, " ")
+        for j in 1:n_cols
+            matrix_string = string("", matrix[i, j], "  ")
+            write(f, matrix_string)
+        end
+        write(f, "\n")
+    end
+    close(f)
+end
+
+"""
+    save_adjacency_matrix(matrix::Matrix{Float32}, filepath_matrix::AbstractString)
+
+# Arguments:
+
+# Return value:
+
+# Notes:
+"""
+function save_adjacency_matrix(matrix::Matrix{Float32}, filepath_matrix::AbstractString)
+    # Open txt file
+    f = open(filepath_matrix, "w")
+    # Write matrix dimensions
+    n_rows, n_cols = size(matrix)
+    matrix_string = ""
+    for i in 1:n_rows
+        write(f, " ")
+        for j in 1:n_cols
+            matrix_string = string("", matrix[i, j], "  ")
+            write(f, matrix_string)
+        end
+        write(f, "\n")
+    end
+    close(f)
+end
+
 function metagraph_from_dataframe_JHistint(graph_type,
                                   df::DataFrame,
                                   origin::Symbol,
@@ -310,8 +385,6 @@ function metagraph_from_dataframe_JHistint(graph_type,
     vertex_names = leftjoin(vertex_names, subpop_df, on = :name => :Child)
     # Set vertex names and attributes
     attr_names = propertynames(vertex_names[!, Not(:vertex_id)])
-    show(vertex_names)
-    show(attr_names)
     for r in eachrow(vertex_names)
         MetaGraphs.set_props!(mg, r[:vertex_id], Dict([a => r[a] for a in attr_names]))
     end
@@ -319,16 +392,13 @@ function metagraph_from_dataframe_JHistint(graph_type,
     if typeof(edge_attributes) == Symbol
         edge_attributes = Vector{Symbol}([edge_attributes])
     end
-    show(edge_attributes)
     origin_id = Symbol(origin, :_id)
     destination_id = Symbol(destination, :_id)
-
     for e in edge_attributes
         for r in eachrow(df)
             MetaGraphs.set_prop!(mg, MetaGraphs.Edge(r[origin_id], r[destination_id]), e, r[e])
         end
     end
-
     # Weight
     if weight != Symbol()
         for r in eachrow(df)
@@ -337,35 +407,12 @@ function metagraph_from_dataframe_JHistint(graph_type,
                       :weight, r[weight])
         end
     end
-
     # Set name as index (Issue #9)
     set_indexing_prop!(mg, :name)
-
+    println(mg)
     return mg
 end
 
-
-# change signatures (NOT IMPORT)
-function spatial_graph(path_dataframe::String, path_matrix::String, seed::Int, n_cell::Int)
-    mat = readdlm(path_matrix, Float32)
-    df = CSV.File(path_dataframe)
-    n = size(mat, 1) # Numero di nodi
-    # n = 2 * count(x -> x != -1, mat)
-    graph = SimpleWeightedGraph(n)
-    for i in 1:n
-        for j in 1:i
-            if mat[i,j] != -1
-                Graphs.add_edge!(graph, i, j, mat[i,j])
-            end
-        end
-    end
-    abstract_g = Graphs.Graph(graph)
-    show(abstract_g)
-    G_meta = metagraph_from_dataframe(SimpleWeightedGraph, df, "origin", "destination", "weight")
-    return G_meta
-end
-
-# in J-Space.jl
 function spatial_graph(path_dataframe_edges::String, path_dataframe_labels::String)
     df_edges_file = CSV.File(path_dataframe_edges)
     df_edges = DataFrame(df_edges_file)
@@ -376,142 +423,115 @@ function spatial_graph(path_dataframe_edges::String, path_dataframe_labels::Stri
     return G_meta
 end
 
-function plot_lattice_JHistint(G::MetaGraph)
-    mylayout = Spectral(dim=3)
+function plot_lattice_JHistint(G::MetaGraph, Set_mut::Vector{Any}; dim::Int=3)
+    driver_mut, labels, colors = get_drivermut_name_colors(G, Set_mut)
+    mylayout = NetworkLayout.Spectral(dim=3)
     f, ax, p = graphplot(G,
                          layout = mylayout,
-                         node_size = 0.0,
+                         node_size = repeat([5], nv(G)),
+                         edge_width=1.0,
+                         node_color = colors)
+    return f, ax, p, colors
+end
+
+function plot_lattice_metagraph(G::MetaGraph; dim::Int=3)
+    # driver_mut, labels, colors = get_drivermut_name_colors(G, Set_mut)
+    mylayout = NetworkLayout.Spectral(dim=2)
+    f, ax, p = graphplot(G,
+                         layout = mylayout,
+                         node_size = repeat([5], nv(G)),
                          edge_width=1.0)
-     # hidedecorations!(ax)
-     # hidespines!(ax)
-    save("image.png", f)
     return f, ax, p
 end
 
-function plot_lattice(G::SimpleWeightedGraph)
-    mylayout = SquareGrid()
-    f, ax, p = graphplot(G,
-                         layout = mylayout,
-                         node_size = 0.0,
-                         node_color = 1.0)
-    save("image.png", f)
-    return f, ax, p
+function vertex_color(v)
+    props = g.vprops[v]
+    color = get_prop(props, :color, "grey")
+    return color
 end
 
-# in DataFrameAPI.jl
-function find_edges_from_vertex(path_dataframe_edges::String, first_vertex::Int64, second_vertex::Int64)
-    df_edges_file = CSV.File(path_dataframe_edges)
-    df_edges = DataFrame(df_edges_file)
-    # Apply filter
-    filtered_edges = filter(row -> (row.origin == first_vertex && row.destination == second_vertex) ||
-                                   (row.origin == second_vertex && row.destination == first_vertex), df_edges)
-    new_df_edges = DataFrame(filtered_edges)
-    return new_df_edges
-end
-
-function find_neighbors_from_vertex(path_dataframe_edges::String, path_dataframe_labels::String, vertex::Int64)
-    df_edges_file = CSV.File(path_dataframe_edges)
-    df_edges = DataFrame(df_edges_file)
-    df_labels_file = CSV.File(path_dataframe_labels)
-    df_labels = DataFrame(df_labels_file)
-    # Extract neighbors vertex based on df_edges
-    neighbors = unique(vcat(filter(row -> row.origin == vertex, df_edges).destination,
-                            filter(row -> row.destination == vertex, df_edges).origin))
-    # Apply filter row in df_labels based on label field
-    neighbor_labels = filter(row -> row.label in neighbors, df_labels)
-    return neighbor_labels
-end
-
-function find_vertex_from_CI(path_dataframe_labels::String, CI::CartesianIndex)
-    df_labels_file = CSV.File(path_dataframe_labels)
-    df_labels = DataFrame(df_labels_file)
-    position = string(CI)
-    filtered_labels = filter(row -> row.position_label == position, df_labels)
-    df_label_result = DataFrame(filtered_labels)
-    return df_label_result
-end
-
-function find_vertex_from_color(path_dataframe_labels::String, color::Float64)
-    df_labels_file = CSV.File(path_dataframe_labels)
-    df_labels = DataFrame(df_labels_file)
-    filtered_labels = filter(row -> row.color_label == color, df_labels)
-    df_label_result = DataFrame(filtered_labels)
-    return df_label_result
-end
-
-function find_vertex_from_label(path_dataframe_labels::String, label::Int64)
-    df_labels_file = CSV.File(path_dataframe_labels)
-    df_labels = DataFrame(df_labels_file)
-    filtered_labels = filter(row -> row.label == label, df_labels)
-    df_label_result = DataFrame(filtered_labels)
-    return df_label_result
-end
-
-function count_vertex_degree(path_dataframe_labels::String, vertex::Int64)
-    df_labels_file = CSV.File(path_dataframe_labels)
-    df_labels = DataFrame(df_labels_file)
-    outgoing_degree = nrow(filter(row -> row.origin == vertex, df_labels))
-    incoming_degree = nrow(filter(row -> row.destination == vertex, df_labels))
-    return outgoing_degree + incoming_degree
-end
-
-function count_edges(path_dataframe_edges::String)
-    df_edges_file = CSV.File(path_dataframe_edges)
-    df_edges = DataFrame(df_edges_file)
-    num_records = nrow(df_edges)
-    return num_records
-end
-
-function count_vertices(path_dataframe_labels::String)
-    df_labels_file = CSV.File(path_dataframe_labels)
-    df_labels = DataFrame(df_labels_file)
-    num_records = nrow(df_labels)
-    return num_records
+function extract_position(G::MetaGraph)
+    position_array = Point{}[]
+    for v in vertices(g_meta)
+        s = get_prop(g_meta, v, :position_label)
+        coordinates_str = match(r"\((.*)\)", string(s)).captures[1]
+        coordinates = parse.(Int, split(coordinates_str, ", "))
+        x, y ,z = coordinates
+        point = Point{Float64}(x, y, z)
+        push!(position_array, point)
+    end
+    return position_array
 end
 
 link = joinpath(@__DIR__, "..", "input_example_demo", "slideExample1", "SlideExample_mini_1.tif")
+# load slide
+println("LOAD SLIDE ...")
 svs_image = read(link)
-println("LOAD SLIDE ... ")
 img = ImageMagick.load_(svs_image)
-
-println("LOAD SLIDE OK ... ")
 bw = Gray.(img) .> 0.15
 dist = 1 .- distance_transform(feature_transform(bw))
 markers = label_components(dist .< -0.3)
-
-println("APPLY SEGMENTATION ... ")
+# watershed
+println("APPLY SEGMENTATION ...")
 segments = watershed(dist, markers)
+# build segmented slide
+println("BUILD SEGMENTED SLIDE ...")
 labels = labels_map(segments)
 colored_labels = IndirectArray(labels, distinguishable_colors(maximum(labels)))
 masked_colored_labels = colored_labels .* (1 .- bw)
-
-# second method
-# segmented_slide = map(i->get_random_color(i), labels_map(segments)) .* (1 .-bw)
-
-println("BUILD GRAPH ... ")
-# Calcola distanza euclidea tra il numero di pixel nei segmenti i e j
+# build graph
+println("BUILD GRAPH ...")
 weight_fn(i,j) = euclidean(segment_pixel_count(segments,i), segment_pixel_count(segments,j))
-df_label = DataFrame()
-G, vert_map, df_label = region_adjacency_graph(segments, weight_fn)
-filepath_dataframe_label = replace(link, ".tif" => "_dataframe_label.csv")
-CSV.write(filepath_dataframe_label, df_label)
-
-println("BUILD & SAVE ADJACENCY MATRIX ... ")
-nvertices = Graphs.nv(G)
+df_labels = DataFrame()
+G, vert_map, df_labels = region_adjacency_graph(segments, weight_fn)
+nvertices = length(vert_map)
+# save dataframe label as .CSV
+filepath_dataframe_labels = replace(link, r"....$" => "_dataframe_labels.csv")
+CSV.write(filepath_dataframe_labels, df_labels)
+# build and save adjacency matrix
+println("BUILD ADJACENCY MATRIX ...")
 matrix = weighted_graph_to_adjacency_matrix_weight(G, nvertices)
-df_edges = build_dataframe_as_edgelist(matrix)
-filepath_dataframe_edges = replace(link, ".tif" => "_dataframe_edges.csv")
-CSV.write(filepath_dataframe_edges, df_edges)
-
-filepath_matrix = replace(link, ".tif" => "_matrix.txt")
+filepath_matrix = replace(link, r"....$" => ".txt")
 save_adjacency_matrix(matrix, filepath_matrix)
+# build and save dataframe edgelist as .CSV
+println("BUILD DATAFRAME EDGELIST ...")
+df_edges = build_dataframe_as_edgelist(matrix)
+filepath_dataframe_edges = replace(link, r"....$" => "_dataframe_edges.csv")
+CSV.write(filepath_dataframe_edges, df_edges)
+filepath_svg = replace(link, r"....$" => ".svg")
+# filepath_png = replace(link, r"....$" => ".png")
+# svs_png = read(filepath_png)
+# img = ImageMagick.load_(svs_png)
 
 # J-Space
-g_meta = spatial_graph(filepath_dataframe_edges, filepath_dataframe_label)
-plot_lattice_JHistint(G)
+g_meta = spatial_graph(filepath_dataframe_edges, filepath_dataframe_labels)
+
+# Plot metagraph on slide
+# plot_lattice_JHistint(g_meta)
+point_array = extract_position(g_meta)
+show(point_array)
+@svg begin
+    # Drawing(M)
+    background("black")
+    sethue("grey40")
+    Karnak.fontsize(8)
+    drawgraph(g_meta,
+        layout = point_array,
+        # layout = [get_prop(g_meta, v, :position_label) for v in vertices(g_meta)],
+        vertexlabels = [get_prop(g_meta, v, :name) for v in vertices(g_meta)],
+        # vertexlabels = [(degree(g_meta, n) == 1) ? find(n) : "" for n in vertices(g_meta)],
+        # vertexfillcolors =
+        #     [RGB(rand()/2, rand()/2, rand()/2)
+        #       for i in 1:nv(g_meta)],
+        # vertexfillcolors = [RGB(get_prop(g_meta, v, :color_label), get_prop(g_meta, v, :color_label), get_prop(g_meta, v, :color_label)) for v in vertices(g_meta)]
+        vertexfillcolors = [normalize_hue(get_prop(g_meta, v, :color_label)) for v in vertices(g_meta)],
+        # edgelabels = [G.weights[e.src, e.dst] for e in edges(G)]
+    )
+end 600 400 filepath_svg
+
+
 
 # modify function in Start_J_Space
-
 # API for DataFrames to include in J-Space, new file julia for DataFrames API
 #label_1 = 12
 #label_2 = 11
