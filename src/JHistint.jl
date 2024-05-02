4d7818f6c617b1713f224b655dfd12fb7d9473ab
Giuseppe Lorenzo Formisano <g.formisano2@campus.unimib.it>
2024-05-02 17:25:02 +0200

    Added function to download single slide.

--- src/JHistint.jl
+++ src/JHistint.jl
@@ -31,6 +31,7 @@ using VoronoiCells
 using GeometryBasics
 
 ### Exported Functions
+export download_single_slide_from_collection
 export download_single_collection
 export download_all_collection
 export slide_cell_segmentation_without_download
@@ -50,6 +51,66 @@ include("noiseManager.jl")
 include("tessellationManager.jl")
 
 ### Main Functions
+"""
+    download_single_slide_from_collection(collection_name::AbstractString, path_to_save::AbstractString)
+
+Function for downloading histological slides in SOPYHSM_app associated
+with a collection available in TCGA.
+
+# Arguments
+- `collection_name::AbstractString` = Collection of TCGA data to download the
+histological slides.
+- `path_to_save::AbstractString` = Local folder path for saving
+histological slides.
+"""
+function download_single_slide_from_collection(collection_name::AbstractString, path_to_save::AbstractString)
+    DirectoryManager.set_environment()
+    # Check the value of the parameter
+    filepath_collection_list = joinpath(DirectoryManager.CONFIG_DIR, "collections", "collectionlist.json")
+    download_collection_values(filepath_collection_list)
+
+    # List with all possible collection values
+    collection_list = extract_collection_values(filepath_collection_list)
+    collection_name = lowercase(collection_name)
+
+    if collection_name in collection_list
+        # Project Management (TCGA-OR-A5J1, TCGA-OR-A5J2, etc.)
+        filepath_collection = joinpath(DirectoryManager.CONFIG_DIR, "collections", "$(collection_name).json")
+        download_project_infos(filepath_collection, collection_name)
+        project_id = extract_project_id(filepath_collection)
+        filepath_case = joinpath(DirectoryManager.CONFIG_DIR, "cases", "$(collection_name).json")
+        casesID_values, casesNAME_values = getCasesForProject(filepath_case, project_id)
+
+        # Slides Management
+        if isdir(joinpath(path_to_save, "$collection_name"))
+            println("Update data ...")
+        else
+            mkdir(joinpath(path_to_save, "$collection_name"))
+        end
+
+        i, j = casesID_values[1], casesNAME_values[1]
+        single_casesID_values, single_casesNAME_values = getCasesForProject(filepath_case, i)
+        x, y = single_casesID_values[1], single_casesNAME_values[1]
+        if !isdir(joinpath(path_to_save, "$(collection_name)", "$j"))
+            mkdir(joinpath(path_to_save, "$(collection_name)", "$j"))
+        end
+        filepath_slides = joinpath(path_to_save, "$(collection_name)", "$j", "$(y).zip")
+        link_slides = "https://api.digitalslidearchive.org/api/v1/folder/$x/download"
+        download_zip(link_slides, filepath_slides)
+        filepath_svs = extract_slide(filepath_slides)
+        insert_record_DB_SOPHYSM(collection_name,
+                                    j, i, y, x,
+                                    link_slides,
+                                    filepath_slides,
+                                    filepath_svs,
+                                    path_to_save)
+        println("DOWNLOAD Slide complete: CASE NAME = $j - SLIDE ID = $y")
+
+    else
+        return "error"
+    end
+end
+
 """
     download_single_collection(collection_name::AbstractString, path_to_save::AbstractString)
 
